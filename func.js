e6cca565e96f08a6c283038bbf96052d0bc93f7b
WarManWarrior <141147536+WarManWarrior@users.noreply.github.com>
2024-06-20 16:50:26 +0530

    start

---
+++ func.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".name").fadeOut(1);
+    $(".name").fadeIn(1000);
+    $("#forth").fadeOut(1);
+    $("#forth").fadeIn(1500);
+    $("#nameimg").slideDown(500)
+})
